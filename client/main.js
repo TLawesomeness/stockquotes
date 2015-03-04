d0431a376c1443305388b02d95b599a2f71d69e9
Tommyluo <tluo12321@yahoo.com>
2015-03-03 16:13:40 -0800

    progress

--- client/main.js
+++ client/main.js
@@ -1,3 +1,29 @@
+'use strict';
+
 $(document).ready(function() {
+  $('#getquotes').click(getData);
 
 });
+
+function getData() {
+  var inputs = $('#text').val();
+  var symbol = inputs;
+  var array = [];
+  var url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + symbol + '&callback=?';
+  console.log(url);
+
+  if (inputs === '' || isNaN(inputs) === false) {
+    alert('Enter a valid input');
+  } else {
+    $.getJSON(url, function(data) {
+      console.log('data:', data);
+
+    var $div = $('<div>');
+    $div.addClass('dataDesign');
+    var display = $div.html('Name: ' + data.Name + '<br />' + 'Symbol: ' + data.Symbol + '<br />' + 'Quote: ' + data.LastPrice +'<br />' + 'Change: ' + data.Change);
+
+    $('.showData').append(display);
+
+  });
+  }
+};
