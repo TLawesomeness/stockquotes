37741a91ded68ef62438e9d122c6b7e68f757584
Tommyluo <tluo12321@yahoo.com>
2015-03-03 17:17:25 -0800

    almost

--- client/main.js
+++ client/main.js
@@ -2,13 +2,12 @@
 
 $(document).ready(function() {
   $('#getquotes').click(getData);
-
+  // $('#delete').click(deleteData);
 });
 
 function getData() {
   var inputs = $('#text').val();
   var symbol = inputs;
-  var array = [];
   var url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + symbol + '&callback=?';
   console.log(url);
 
@@ -18,12 +17,23 @@ function getData() {
     $.getJSON(url, function(data) {
       console.log('data:', data);
 
+    var array = [];
+    var $del = $('<input type="button" value = "Delete" />');
     var $div = $('<div>');
     $div.addClass('dataDesign');
     var display = $div.html('Name: ' + data.Name + '<br />' + 'Symbol: ' + data.Symbol + '<br />' + 'Quote: ' + data.LastPrice +'<br />' + 'Change: ' + data.Change);
-
+    array.push(display);
+    display.append($del);
     $('.showData').append(display);
+
+    $del.click(function() {
+      $(this).remove();
+    })
 
   });
   }
 };
+
+// function deleteData() {
+//   $('.dataDesign').remove();
+// }
